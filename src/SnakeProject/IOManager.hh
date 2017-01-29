90625e4baa13c79adb69ddfa47760efbe9dd6759
Daniel Dominguez Hernandez <danieldominguezhernandez@hotmail.com>
2017-01-29 02:00:01 +0100

    Finalmente conexiones 100% funcionales.

--- src/SnakeProject/IOManager.hh
+++ src/SnakeProject/IOManager.hh
@@ -7,49 +7,45 @@
 #include <fstream>
 #include <string>
 
+#define archivoRanking "ranking.dat"
+#define archivoConfiguacion "configuracion.dat"
+
 using namespace std;
 
 namespace IOManager {
 	void openingConfiguration(string difficulty) {
-		rapidxml::file<> xmlFile("myXML.xml");
+		rapidxml::file<> xmlFile(archivoConfiguacion);
 		rapidxml::xml_document<> doc;
 		doc.parse<0>(xmlFile.data());
 		std::cout << "Nombre de la raiz: " << doc.first_node()->name() << "\n";
 	}
 
 	void introducirRanking(string nombre, int score) {
-		const char* nombrein = nombre.c_str();
+		char final = ';';
 		int scorein = score;
-		char separacion = ';';
-		char* nombreout = {};
+
 		ofstream fsalida("ranking.dat", ios::out | ios::app | ios::binary);
 
-		fsalida.write(reinterpret_cast<char *>(&nombrein), sizeof(nombrein));
+		fsalida.write((nombre.c_str()), nombre.size());
+		fsalida.write(reinterpret_cast<char *>(&final), sizeof(final));
 		fsalida.write(reinterpret_cast<char *>(&scorein), sizeof(scorein));
 
 		fsalida.close();
-
-		/*ifstream fentrada("ranking.dat", ios::in | ios::binary);
-
-		fentrada.read(reinterpret_cast<char *>(&nombreout), sizeof(nombreout));
-
-		fentrada.close();
-
-		cout << nombreout << endl;*/
 	}
 	
-	/*vector<pair<char*, int>>*/ void leerRanking() {
-		vector<pair<char*, int>> ranking;
+	vector<pair<string, int>> leerRanking() {
+		vector<pair<string, int>> ranking;
+
 		bool cond = false;
-		char* pruebachar;
+		string pruebachar;
 		int pruebaint;
 
-		ifstream fentrada("ranking.dat", ios::in | ios::binary);
+		ifstream fentrada(archivoRanking, ios::in | ios::binary);
 
 		if (fentrada.is_open()) {
 			while (!fentrada.eof()) {
 				if (!cond) {
-					fentrada.read(reinterpret_cast<char *>(&pruebachar), sizeof(pruebachar));
+					getline(fentrada, pruebachar, ';');
 					cond = true;
 				} else {
 					fentrada.read(reinterpret_cast<char *>(&pruebaint), sizeof(pruebaint));
@@ -61,37 +57,8 @@ namespace IOManager {
 		else {
 			cout << "No hay ninguna puntuacion" << endl;
 		}
-
 		fentrada.close();
 
-		for (auto it = ranking.begin(); it != ranking.end(); ++it) {
-			auto v_temp = *it;
-			std::cout << v_temp.second << endl;
-		
-		}
-
-		//return ranking;
-	}
-
-	void introducirRanking2(string nombre, int score) {
-		const char* nombrein = nombre.c_str();
-		int scorein = score, scoreout;
-		char* nombreout = {};
-
-		ofstream fsalida("ranking.dat", ios::out | ios::binary);
-
-		fsalida.write(reinterpret_cast<char *>(&nombrein), sizeof(nombrein));
-		fsalida.write(reinterpret_cast<char *>(&scorein), sizeof(scorein));
-
-		fsalida.close();
-
-		ifstream fentrada("ranking.dat", ios::in | ios::binary);
-
-		fentrada.read(reinterpret_cast<char *>(&nombreout), sizeof(nombreout));
-		fentrada.read(reinterpret_cast<char *>(&scoreout), sizeof(scoreout));
-
-		fentrada.close();
-
-		cout << nombreout << ' ' << scoreout << endl;
+		return ranking;
 	}
 }
--- src/SnakeProject/main.cpp
+++ src/SnakeProject/main.cpp
@@ -14,8 +14,13 @@
 int main(int argc, char* args[]) {
 	//IOManager::openingConfiguration();
 	IOManager::introducirRanking("Carlos", 9999);
-	IOManager::leerRanking();
-	//IOManager::introducirRanking2("carlos", 9999);
-	GameEngine::Run<SCREEN_WIDTH, SCREEN_HEIGHT>("Snake");
+	vector<pair<string, int>> ranking = IOManager::leerRanking();
+
+	for (auto it = ranking.begin(); it != ranking.end(); ++it) {
+		auto v_temp = *it;
+		std::cout << v_temp.first << ' ' << v_temp.second << endl;
+	}
+
+	//GameEngine::Run<SCREEN_WIDTH, SCREEN_HEIGHT>("Snake");
 	return 0;
 }
